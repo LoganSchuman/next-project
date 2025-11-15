06789a1b8a2706cb69fd9c0c4c1a8b653e2118b9
Logan Schuman <loganschuman406@gmail.com>
2025-11-14 16:16:07 -0800

    added final def

--- app/components/ProjectInfo.js
+++ app/components/ProjectInfo.js
@@ -7,8 +7,8 @@ export default function ProjectInfo() {
     <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px 0' }}>
       <h3>Final Project Idea</h3>
       <p>
-        My web topic for the final project is... [describe your topic].
-        I'd like my project to showcase this by... [describe how you will build it].
+        My web topic for the final project is to create a website for a horse riding business for a mutual friend between my project partner and I.
+        I'd like my project to showcase this by making a full stack applicaiton using Next.js. We will most likely be using React components for the UI as well as phtoo galleries, contact information, etc. Next.js will be very important for the backend.
       </p>
       
       <Link href={repoUrl} target="_blank" rel="noopener noreferrer">
